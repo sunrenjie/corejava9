7ad99dea3cfcef6122ffeffa4d4aa8a835433b58
Renjie Sun <sunrenjie6@gmail.com>
2015-09-23 15:38:06 +0800

    [NotHelloWorld] Improved text-displaying a bit
    These features are implemented:
    1. The text is always displayed at (roughly) center of the window no
       matter how it is resized.
    2. Text font (along with the size, style) and color is parameterized.

--- v1ch07/notHelloWorld/NotHelloWorld.java
+++ v1ch07/notHelloWorld/NotHelloWorld.java
@@ -5,6 +5,9 @@ import javax.swing.JComponent;
 import java.awt.EventQueue;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Color;
 
 /**
  * @version 1.32 2007-06-12
@@ -18,7 +21,8 @@ public class NotHelloWorld
          {
             public void run()
             {
-               JFrame frame = new NotHelloWorldFrame();
+               JFrame frame = new NotHelloWorldFrame(
+                       new Font("Times New Roman", 0, 12), Color.RED);
                frame.setTitle("NotHelloWorld");
                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                frame.setVisible(true);
@@ -32,9 +36,9 @@ public class NotHelloWorld
  */
 class NotHelloWorldFrame extends JFrame
 {
-   public NotHelloWorldFrame()
+   public NotHelloWorldFrame(Font font, Color color)
    {
-      add(new NotHelloWorldComponent());
+      add(new NotHelloWorldComponent(font, color));
       pack();
    }
 }
@@ -49,10 +53,28 @@ class NotHelloWorldComponent extends JComponent
 
    private static final int DEFAULT_WIDTH = 300;
    private static final int DEFAULT_HEIGHT = 200;
+   private static final String text = "Not a Hello, World program";
+
+   private Font textFont;
+   private Color textColor;
+
+   public NotHelloWorldComponent(Font font, Color color)
+   {
+      textFont = font;
+      textColor = color;
+   }
 
    public void paintComponent(Graphics g)
    {
-      g.drawString("Not a Hello, World program", MESSAGE_X, MESSAGE_Y);
+      g.setFont(textFont);
+      g.setColor(textColor);
+      int widthText = g.getFontMetrics(textFont).stringWidth(text);
+      int width = this.getWidth();
+      // Try to put the text in the middle. Text height, considered relatively
+      // small, is ignored. Stolen from:
+      // http://stackoverflow.com/questions/18483768/draw-text-in-middle-of-the-screen
+      int left = width < widthText ? 0 : (width - widthText) / 2;
+      g.drawString(text, left, this.getHeight() / 2);
    }
    
    public Dimension getPreferredSize() { return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT); }
